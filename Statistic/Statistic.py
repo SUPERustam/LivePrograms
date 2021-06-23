fa3d8245febca033922364b16715ceb54a9788d7
SUPERustam <498rustam@gmail.com>
2021-06-23 12:38:27 +0300

    Automated sync by sync shell script

--- Statistic/Statistic.py
+++ Statistic/Statistic.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def main():
     fig, ax = plt.subplots()  # ax - axes or array of axes
-    y = np.array([1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 2, 0, 1, 0, 2, 3, 2, 2, 3, 2])
+    y = np.array([1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 2, 0, 1, 0, 2, 3, 2, 2, 3, 2, 2, 2])
     x = np.array(range(1, len(y) + 1))
     schedule_y = np.array([2, 3])
     schedule_x = np.array([15, 16])
@@ -13,7 +13,7 @@ def main():
     ax.scatter(schedule_x, schedule_y, color='red', marker='P', label='days, when i use schedule')
     ax.set_title("Tasks performance")
     ax.set_ylabel('Done tasks')
-    ax.set_xlabel('Days, first day == 2 June')
+    ax.set_xlabel(f'{len(y)} Days, first day == 2 June')
     ax.legend()
 
     fig.show()
