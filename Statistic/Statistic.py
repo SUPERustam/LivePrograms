7940fa71f4659d89b7491c4e055eba41c846a5a9
SUPERustam <498rustam@gmail.com>
2021-06-26 19:35:20 +0300

    Automated sync by sync shell script

--- Statistic/Statistic.py
+++ Statistic/Statistic.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def main():
     fig, ax = plt.subplots()  # ax - axes or array of axes
-    y = np.array([1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 2, 0, 1, 0, 2, 3, 2, 2, 3, 2, 2, 2])
+    y = np.array([1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 2, 0, 1, 0, 2, 3, 2, 2, 3, 2, 2, 2, 2, 2, 2, 3
+                  ])
     x = np.array(range(1, len(y) + 1))
     schedule_y = np.array([2, 3])
     schedule_x = np.array([15, 16])
@@ -22,4 +23,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
